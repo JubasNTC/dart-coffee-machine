28da70d77020f5e42f58ebebdee9311d83b33e45
JubasNTC <31780672+JubasNTC@users.noreply.github.com>
2019-11-12 22:51:52 +0300

    create check cash method

--- CoffeeMachine.dart
+++ CoffeeMachine.dart
@@ -2,8 +2,20 @@ class CoffeeMachine {
   int _balance;
 
   CoffeeMachine(int balance) {
+    if (balance < 0) {
+      throw Error();
+    }
     this._balance = balance;
   }
 
-  getBalance() => this._balance;
+  int getBalance() => this._balance;
+
+  void addCash(int cash) => {
+    if (isCash(cash)) {
+      this._balance += cash
+    }
+  };
+
+  bool isCash(int cash) => cash == 1 || cash == 2 || cash == 5 || cash == 10 ||
+                           cash == 50 || cash == 100;
 }
